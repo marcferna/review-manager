1009a0031ac95337eeacab295c214807c7bbfe60
Marc Fernandez <marc.fernandezg@gmail.com>
2016-06-04 23:13:25 -0700

    Migrate to postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,70 +13,73 @@
 
 ActiveRecord::Schema.define(version: 20160522055951) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "apps", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "type",            limit: 255
-    t.string   "uid",             limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "organization_id", limit: 4
+    t.string   "name"
+    t.string   "type"
+    t.string   "uid"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "organization_id"
   end
 
   add_index "apps", ["organization_id"], name: "index_apps_on_organization_id", using: :btree
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "uid",        limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "organizations", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "app_id",     limit: 4
-    t.string   "uid",        limit: 255
-    t.string   "title",      limit: 255
-    t.text     "text",       limit: 65535
-    t.integer  "rating",     limit: 4
-    t.string   "version",    limit: 255
-    t.integer  "author_id",  limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "app_id"
+    t.string   "uid"
+    t.string   "title"
+    t.text     "text"
+    t.integer  "rating"
+    t.string   "version"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "reviews", ["app_id"], name: "index_reviews_on_app_id", using: :btree
   add_index "reviews", ["author_id"], name: "index_reviews_on_author_id", using: :btree
 
   create_table "slack_integrations", force: :cascade do |t|
-    t.integer  "organization_id", limit: 4
-    t.string   "webhook_url",     limit: 255
-    t.string   "channel",         limit: 255
-    t.boolean  "active",                      default: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
+    t.integer  "organization_id"
+    t.string   "webhook_url"
+    t.string   "channel"
+    t.boolean  "active",          default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   add_index "slack_integrations", ["organization_id"], name: "index_slack_integrations_on_organization_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.integer  "organization_id",        limit: 4
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "organization_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
